47d6131d99e108e86422e3a321d4756a06b11db3
Andy PearTree <andorelouise@gmail.com>
2023-05-04 19:08:45 +0100

    Update TerminalEchoesGPT.py

--- TerminalEchoesGPT.py
+++ TerminalEchoesGPT.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request
 from nomic.gpt4all import GPT4All
 
 app = Flask(__name__)
+gpt = None
 
 @app.route('/')
 def main():
@@ -9,13 +10,15 @@ def main():
 
 @app.route('/prompt')
 def generate_prompt():
+    global gpt
+    if gpt is None:
+        gpt = GPT4All()
+        gpt.open()
     prompt = request.args.get('prompt')
     if not prompt:
         return jsonify({'error': 'No prompt provided'})
-    response = m.prompt(prompt)
+    response = gpt.prompt(prompt)
     return jsonify({'prompt': prompt, 'response': response})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
-    m = GPT4All()
-    m.open()
